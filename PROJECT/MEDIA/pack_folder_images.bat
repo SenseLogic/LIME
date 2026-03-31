649c5ed9d1201bdd06af3f64d97e27aea960e08f
SenseLogic <dev.senselogic@gmail.com>
2026-03-31 22:00:08 +0200

    % media

--- PROJECT/MEDIA/pack_folder_images.bat
+++ PROJECT/MEDIA/pack_folder_images.bat
@@ -1,8 +1,6 @@
-for %%f in (static\image\%1\*.png) do (
-    %TOOL%\IMAGE_MAGICK\magick "%%f" -auto-orient -quality 100 -define heic:lossless=true -strip "static\image\%1\%%~nf.avif"
-    del /q "%%f"
-)
-for %%f in (static\image\%1\*.jpg) do (
-    %TOOL%\IMAGE_MAGICK\magick "%%f" -auto-orient -quality 100 -define heic:lossless=true -strip "static\image\%1\%%~nf.avif"
-    del /q "%%f"
+for %%f in ("static\image\%1\*.png" "static\image\%1\*.jpg") do (
+    %TOOL%\LIBAVIF\avifenc --lossless --ignore-exif --ignore-xmp "%%f" "static\image\%1\%%~nf.avif"
+    if exist "static\image\%1\%%~nf.avif" (
+        del /q "%%f"
+    )
 )
