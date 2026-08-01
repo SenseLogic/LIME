62e383f904dff08c9e2434a3fdde6f27d67bf49c
SenseLogic <dev.senselogic@gmail.com>
2026-08-01 10:47:56 +0200

    % script

--- PROJECT/update_rules.bat
+++ PROJECT/update_rules.bat
@@ -1,2 +1,2 @@
 mkdir .cursor\rules
-%TOOL%\AEGYS\aegys %TOOL%\AEGYS\IN\coding-standards.mdc "format=Cursor language=TypeScript styling_language=SCSS framework=Fresh greenfield" .cursor\rules\typescript-coding-standards.mdc
+%TOOL%\AEGYS\aegys %TOOL%\AEGYS\IN\coding-standards.mdc "format=Cursor language=TypeScript styling_language=SCSS framework=Fresh greenfield" .cursor\rules\coding-standards.mdc
